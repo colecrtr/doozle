be2c5b21082b5e5ea70d5e69cd881c666a96bb18
Cole Carter <coleccarter@gmail.com>
2020-04-25 11:35:45 -0600

    Add site-wide header

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
+import Header from "./Header";
 
+import "bulma";
 import './App.scss';
 
 class App extends React.PureComponent {
     render() {
         return (
             <React.StrictMode>
+                <Header />
                 <div>
-                    <h1>Helo World!</h1>
+                    <h1>Hello World!</h1>
                 </div>
             </React.StrictMode>
         );
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
                     loader: 'babel-loader'
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ]
     },
     plugins: [
