2ef104e70672f3616a3d4afd0bca3d0562257f16
Cole Carter <coleccarter@gmail.com>
2020-04-24 21:07:11 -0600

    Add react web app boilerplate
    in TypeScript, which is new to me so this should be fun ...

---
+++ web/src/components/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import './App.scss';
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <React.StrictMode>
+                <div>
+                    <h1>Helo World!</h1>
+                </div>
+            </React.StrictMode>
+        );
+    }
+}
+
+export default App;
