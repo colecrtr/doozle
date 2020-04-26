675f417052107b2006cce460908bc87f0d2e7f6e
Cole Carter <coleccarter@gmail.com>
2020-04-25 20:33:56 -0600

    Show Login/Register header link on smaller screens

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
-import { Navbar, NavbarItem, NavbarBrand, NavbarMenu, NavbarEnd, Button } from "bloomer";
+import { Navbar, NavbarItem, NavbarBrand, NavbarMenu, NavbarEnd, Button, NavbarBurger } from "bloomer";
 
+interface IState {
+    isBurgerMenuActive: Boolean,
+}
 
-export default class Header extends React.Component {
+export default class Header extends React.Component<any, IState> {
+    readonly state = { isBurgerMenuActive: false };
+
+    toggleBurgerMenu() {
+        this.setState({ isBurgerMenuActive: !this.state.isBurgerMenuActive });
+    }
+
     render() {
         return (
             <header>
                 <Navbar>
                     <NavbarBrand>
                         <NavbarItem style={{ fontSize: "1.25rem" }}>Doozle</NavbarItem>
+                        <NavbarBurger isActive={this.state.isBurgerMenuActive} onClick={this.toggleBurgerMenu.bind(this)} />
                     </NavbarBrand>
-                    <NavbarMenu>
+                    <NavbarMenu isActive={this.state.isBurgerMenuActive} onClick={this.toggleBurgerMenu.bind(this)} >
                         <NavbarEnd>
-                            <NavbarItem><Button href="/session">Login / Register</Button></NavbarItem>
+                            <NavbarItem><Button href="/session" isColor="success">Login / Register</Button></NavbarItem>
                         </NavbarEnd>
                     </NavbarMenu>
                 </Navbar>
