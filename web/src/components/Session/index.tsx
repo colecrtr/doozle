5babec2a25f8530471a84e3f6dfc91ee12eca941
Cole Carter <coleccarter@gmail.com>
2020-05-13 18:30:54 -0600

    Migrate to Switch because a BrowserRouter is already in the parent component

--- web/src/components/Session/index.tsx
+++ web/src/components/Session/index.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import Authenticate from "./Authenticate";
 
 export default class Session extends React.Component {
     render() {
         return (
-            <BrowserRouter>
+            <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/authenticate" component={Authenticate} />
-            </BrowserRouter>
+            </Switch>
         );
     }
 }
