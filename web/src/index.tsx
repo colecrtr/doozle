d01fe43ed2048620b26313d52451037aa2f7dda5
Cole Carter <coleccarter@gmail.com>
2020-05-24 11:52:05 -0600

    Try to resolve issue with Sentry not logging any errors

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,7 +8,6 @@ Sentry.init({
   dsn:
     "https://6e461e34086b42749c1aa092a966bfbe@o205093.ingest.sentry.io/5251897",
   environment: isProduction ? "production" : "development",
-  enabled: isProduction,
 });
 
 ReactDOM.render(<App />, document.getElementById("root"));
