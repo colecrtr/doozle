8d8eec16600b91df7044e3956bc77185cc754a5c
Cole Carter <coleccarter@gmail.com>
2020-05-24 11:37:35 -0600

    Improve formatting

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/App";
 import * as Sentry from "@sentry/browser";
 import { isProduction } from "helpers/consts";
+import App from "./components/App";
 
 Sentry.init({
   dsn:
